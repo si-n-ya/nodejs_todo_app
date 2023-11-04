2690d8dc3ede2bd97b5baf828e1fa089cd9d9b4f
github user si-n-ya <sin.982.9.s@gmail.com>
2023-11-05 00:55:29 +0900

    タスクの登録処理（DBなしで一旦コミット）

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,73 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+type Task = {
+  id: number;
+  title: string;
+  is_done?: number;
+};
+
+type AddTask = {
+  title: string;
+};
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState('')
+
   useEffect(() => {
     axios
       .get("http://localhost:3000")  // ローカルのバックエンドサーバーのURLにgetメソッドでアクセス
       .then((response) => {
-        console.log(response.data.todos)
+        console.log(response.data.tasks);
+        const { tasks } = response.data;
+        setTasks(tasks);
       })
       .catch((e) => {
         console.log(e.message);
       });
   }, []);
 
-  return <div></div>;
+  const addTask = async (title: string) => {
+    console.log(title);
+    await axios
+      .post<AddTask>(
+        "http://localhost:3000/add",
+        { title: title },
+      )
+      .then((response) => {
+        console.log(response.data);
+        const data = response.data;
+        // setTasks((preTasks) => [Task, ...preTasks]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(title)
+    addTask(title)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="TODOを入力してください。"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}  
+        />
+        <button type="submit">add</button>
+      </form>
+      {tasks.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+    </>
+  );
 }
 
 export default App
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { pool } from './database'; // ここでデータベース接続関数をインポート
+import { pool } from './database';
 
 const app: express.Express = express()
 const PORT = 3000;
 
 app.use(cors({ origin: "http://localhost:5174" }));
+// フロントから送られてきたjsonデータをバックエンドで利用できるようにする
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req: Request, res: Response) => {
   console.log("getリクエストテスト");
   try {
     const [rows] = await pool.query('SELECT id, title, is_done FROM tasks');
-    return res.status(200).json({ todos: rows });
+    return res.status(200).json({ tasks: rows });
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).send(error.message);
     }
   }
+})
+
+app.post("/add", (req: Request, res: Response) => {
+  console.log("task追加リクエスト");
+  console.log(req.body);
+  const { title } = req.body;
+  return res.status(200).json({ title: title });
 })
 
 try {
