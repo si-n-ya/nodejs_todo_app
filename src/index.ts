fb387c415b7faf67ff810d7bae7d49408bbca17e
github user si-n-ya <sin.982.9.s@gmail.com>
2023-11-04 13:13:58 +0900

    npm-scriptsにdevとdev:watchを追加。Node.jsのHTTPモジュールを使用して簡易なHTTPサーバーを作成・サンプル用にGETリクエストの処理

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("test node.js");
+import http from 'http'; // httpモジュールの読み込み
+
+const server = http.createServer((req, res) => { //  HTTPサーバを作成
+  if (req.url === '/todos' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' }); // レスポンスHTTPヘッダーを設定
+    res.end(JSON.stringify({ message: 'todoテスト' })); // レスポンスボディを送信
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'ページが見つかりません。' }));
+  }
+});
+
+const PORT = process.env.PORT || 3000; // ポート3000でリクエストを行う
+
+server.listen(PORT, () => console.log(`サーバー起動中：ポート ${PORT}`));
